a51175c61337161be03ce45dee98127ec68fff2b
Rodrigo Martins <rodrigomartins@msn.com>
2025-01-24 18:41:01 +0000

    deving html for event

--- ktor-server/src/main/kotlin/example/com/web/components/post/_event.kt
+++ ktor-server/src/main/kotlin/example/com/web/components/post/_event.kt
@@ -1,34 +1,47 @@
 package example.com.web.components.post
 
 import example.com.data.db.event.Event
+import example.com.data.utils.LocalDateTimeSerializer
 import kotlinx.html.HtmlBlockTag
 import kotlinx.html.div
 import kotlinx.html.img
 import kotlinx.html.p
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 fun HtmlBlockTag.event(event: Event){
-    div(classes = "flex flex-col items-center justify-center w-full p-4") {
-        div(classes = "w-full p-4 bg-white rounded-lg shadow-lg") {
-            div(classes = "flex flex-col items-center justify-center w-full") {
-                div(classes = "flex flex-row items-center justify-between w-full") {
-                    div(classes = "flex flex-row items-center") {
-                        img(classes = "w-12 h-12 rounded-full") {
-                            src = "/resources/images/profile.png"
-                        }
-                        div(classes = "flex flex-col items-start justify-center ml-4") {
-                            p(classes = "text-lg font-bold") {
-                                +event.title
-                            }
-                            p(classes = "text-sm text-gray-500") {
-                                +event.date.toString()
-                            }
-                        }
+    val date = LocalDateTime.parse(event.date.toString()).format(DateTimeFormatter.ofPattern("dd MMM"))
+    val dayOfWeek = LocalDateTime.parse(event.date.toString()).dayOfWeek.toString()
+
+    div(classes = "flex flex-row items-center justify-center w-full p-4 rounded-xl") {
+        // date div
+        div(classes = "flex flex-col flex-1 items-center justify-center text-center text-sm") {
+            p(classes = "text-lg font-bold") {
+                +date
+            }
+            p(classes = "text-sm text-gray-500") {
+                +dayOfWeek
+            }
+        }
+
+        div(classes = "flex w-[80%] p-4 bg-white rounded-xl shadow-lg") {
+            div(classes = "flex flex-row items-start justify-between w-full") {
+                div(classes = "flex flex-col items-start justify-center ml-4") {
+                    p(classes = "text-lg font-bold") {
+                        +event.title
+                    }
+                    p(classes = "text-sm text-gray-500") {
+                        +event.date.toString()
                     }
                 }
-                p(classes = "text-lg mt-4") {
-                    +event.description
+                // image div
+                div(classes = "flex flex-row items-center") {
+                    img(classes = "w-[200px] h-[100px] rounded-2xl") {
+                        src = "/resources/uploads/images/0e396674-6989-473f-a61c-ae4c748006a2.jpeg"
+                    }
                 }
             }
         }
+
     }
 }
