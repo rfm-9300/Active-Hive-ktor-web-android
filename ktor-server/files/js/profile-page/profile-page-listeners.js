bcc3294b88c32ed218a54bf4e8e09a5632bdd44a
Rodrigo Martins <rodrigomartins@msn.com>
2025-02-28 16:41:15 +0000

    updated for deployment

---
+++ ktor-server/files/js/profile-page/profile-page-listeners.js
@@ -0,0 +1,18 @@
+
+// indication for file loaded
+document.getElementById("image").addEventListener("change", function() {
+  const fileInput = this;
+  const file = fileInput.files[0];
+
+  // Reference to the status element
+  const uploadStatus = document.getElementById("upload-status");
+
+  if (file) {
+    // Update the text to show the filename
+    const fileName = file.name.length > 10 ? file.name.substring(0, 10) + "..." : file.name;
+    uploadStatus.textContent = `"${fileName}" selected!`;
+  } else {
+    // Clear the text if no file is selected
+    uploadStatus.textContent = "";
+  }
+});
